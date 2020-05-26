b60ab1b0a99ba7a626aa14593372355d58f1b9e0
unknown <DarshanBaranwal@MDXN00110374.bku.db.de>
2020-05-26 08:17:59 +0200

    Leap Year Test cases added

--- src/test/java/com/leapYear/testCase/LeapYearTest.java
+++ src/test/java/com/leapYear/testCase/LeapYearTest.java
@@ -1,5 +1,22 @@
 package com.leapYear.testCase;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
 
 public class LeapYearTest {
-
+	Leapyear lp = new Leapyear();
+	
+	@Test
+	public void isLeapYear() {
+		assertEquals(true, lp.isLeapYear(2020));
+	}
+	
+	@Test
+	public void isMilleniumLeapYear() {
+		assertEquals(false, lp.isLeapYear(1000));
+	}
+	
+	@Test
+	public void is400MultipleMilleniumYearLeapyear() {
+		assertEquals(true, lp.isLeapYear(2000));
+	}
 }
